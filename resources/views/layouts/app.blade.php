1bee7993d56f051a139cdacc87a6404f1b5378f0
David Flores <dflores@jed-itsolutions.com>
2017-05-16 16:34:36 -0600

    Cambios segundo formulario

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,15 +9,15 @@
 
 
 
-<!-- Formulario-->
+<!-- Formulario Sección Lider-->
 <link rel="stylesheet" href="/css/styleheader.css" type="text/css" />
 <script type="text/javascript" src="formprueba_files/formoid1/jquery.min.js"></script>
 <form class="formoid-solid-red" style="background-color:#FFFFFF;font-size:14px;font-family:'Roboto',Arial,Helvetica,sans-serif;color:#34495E;max-width:480px;min-width:150px" method="post"><div class="title"><h2>Honduras Startup - MiPymes</h2></div>
-    <div class="element-number" title="Número de Identidad"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="medium" type="text" min="0" max="18" name="number" required="required" placeholder="Número de Identidad" value=""/><span class="icon-place"></span></div></div>
-    <div class="element-name"><label class="title"><span class="required">*</span></label><span class="nameFirst"><input placeholder="Nombre Primer" type="text" size="8" name="name[first]" required="required"/><span class="icon-place"></span></span><span class="nameLast"><input placeholder="Nombre Segundo" type="text" size="14" name="name[last]" required="required"/><span class="icon-place"></span></span></div>
-    <div class="element-input" title="Primer Apellido"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="large" type="text" name="input1" required="required" placeholder="Primer Apellido"/><span class="icon-place"></span></div></div>
-    <div class="element-input" title="Segundo Apellido"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="large" type="text" name="input2" required="required" placeholder="Segundo Apellido"/><span class="icon-place"></span></div></div>
-    <div class="element-date" title="Fecha de Nacimiento"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="large" data-format="yyyy-mm-dd" type="date" name="date" required="required" placeholder="Fecha de Nacimiento"/><span class="icon-place"></span></div></div>
+    <div class="element-number" title="Número de Identidad Lider"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="medium" type="text" min="0" max="18" name="number" required="required" placeholder="Número de Identidad Lider" value=""/><span class="icon-place"></span></div></div>
+    <div class="element-name"><label class="title"><span class="required">*</span></label><span class="nameFirst"><input placeholder="Primer Nombre Lider" type="text" size="8" name="name[first]" required="required"/><span class="icon-place"></span></span><span class="nameLast"><input placeholder="Segundo Nombre Lider" type="text" size="14" name="name[last]" required="required"/><span class="icon-place"></span></span></div>
+    <div class="element-input" title="Primer Apellido"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="large" type="text" name="input1" required="required" placeholder="Primer Apellido Lider"/><span class="icon-place"></span></div></div>
+    <div class="element-input" title="Segundo Apellido"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="large" type="text" name="input2" required="required" placeholder="Segundo Apellido Lider"/><span class="icon-place"></span></div></div>
+    <div class="element-date" title="Fecha de Nacimiento"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="large" data-format="yyyy-mm-dd" type="date" name="date" required="required" placeholder="Fecha de Nacimiento Lider"/><span class="icon-place"></span></div></div>
     <div class="element-radio" title="Género"><label class="title">Género<span class="required">*</span></label>        <div class="column column2"><label><input type="radio" name="radio" value="Masculino    " required="required"/><span>Masculino  </span></label></div><span class="clearfix"></span>
         <div class="column column2"><label><input type="radio" name="radio" value="Femenino" required="required"/><span>Femenino</span></label></div><span class="clearfix"></span>
 </div>
@@ -52,10 +52,59 @@
         <option value="Postgrado">Postgrado</option>
         <option value="Doctorado">Doctorado</option>
         <option value="Otro">Otro</option></select><i></i><span class="icon-place"></span></span></div></div></div>
-    <div class="element-email" title="Correo Electrónico"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="large" type="email" name="email" value="" required="required" placeholder="Correo Electrónico"/><span class="icon-place"></span></div></div>
-    <div class="element-phone" title="Teléfono Fijo"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="large" type="tel" pattern="[+]?[\.\s\-\(\)\*\#0-9]{3,}" maxlength="24" name="phone" required="required" placeholder="Teléfono Fijo" value=""/><span class="icon-place"></span></div></div>
-    <div class="element-phone" title="Teléfono Celular"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="large" type="tel" pattern="[+]?[\.\s\-\(\)\*\#0-9]{3,}" maxlength="24" name="phone1" required="required" placeholder="Teléfono Celular" value=""/><span class="icon-place"></span></div></div>
-    <div class="element-input" title="Dirección Exacta"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="large" type="text" name="input3" required="required" placeholder="Dirección Exacta"/><span class="icon-place"></span></div></div>
+    <div class="element-email" title="Correo Electrónico"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="large" type="email" name="email" value="" required="required" placeholder="Correo Electrónico Lider"/><span class="icon-place"></span></div></div>
+    <div class="element-phone" title="Teléfono Fijo"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="large" type="tel" pattern="[+]?[\.\s\-\(\)\*\#0-9]{3,}" maxlength="24" name="phone" required="required" placeholder="Teléfono Fijo Lider" value=""/><span class="icon-place"></span></div></div>
+    <div class="element-phone" title="Teléfono Celular"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="large" type="tel" pattern="[+]?[\.\s\-\(\)\*\#0-9]{3,}" maxlength="24" name="phone1" required="required" placeholder="Teléfono Celular Lider" value=""/><span class="icon-place"></span></div></div>
+    <div class="element-input" title="Dirección Exacta"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="large" type="text" name="input3" required="required" placeholder="Dirección Exacta Lider"/><span class="icon-place"></span></div></div>
+
+<div class="submit"><input type="submit" value="Enviar"/></div></form><p class="frmd"><a href="">Validación Formulario</a> Hondurastartup MiPyme Form</p>
+<!-- Finaliza Formulario-->
+
+<!-- Formulario Sección Integrantes-->
+<form class="formoid-solid-red" style="background-color:#FFFFFF;font-size:14px;font-family:'Roboto',Arial,Helvetica,sans-serif;color:#34495E;max-width:480px;min-width:150px" method="post"><div class="title"><h2>Datos de Integrantes</h2></div>
+    <div class="element-number" title="Número de Identidad Integrante"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="medium" type="text" min="0" max="18" name="number" required="required" placeholder="Número de Identidad Integrante" value=""/><span class="icon-place"></span></div></div>
+    <div class="element-name"><label class="title"><span class="required">*</span></label><span class="nameFirst"><input placeholder="Primer Nombre Integrante" type="text" size="8" name="name[first]" required="required"/><span class="icon-place"></span></span><span class="nameLast"><input placeholder="Segundo Nombre Integrante" type="text" size="14" name="name[last]" required="required"/><span class="icon-place"></span></span></div>
+    <div class="element-input" title="Primer Apellido"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="large" type="text" name="input1" required="required" placeholder="Primer Apellido Integrante"/><span class="icon-place"></span></div></div>
+    <div class="element-input" title="Segundo Apellido"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="large" type="text" name="input2" required="required" placeholder="Segundo Apellido Integrante"/><span class="icon-place"></span></div></div>
+    <div class="element-date" title="Fecha de Nacimiento"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="large" data-format="yyyy-mm-dd" type="date" name="date" required="required" placeholder="Fecha de Nacimiento Integrante"/><span class="icon-place"></span></div></div>
+    <div class="element-radio" title="Género"><label class="title">Género<span class="required">*</span></label>        <div class="column column2"><label><input type="radio" name="radio" value="Masculino    " required="required"/><span>Masculino  </span></label></div><span class="clearfix"></span>
+        <div class="column column2"><label><input type="radio" name="radio" value="Femenino" required="required"/><span>Femenino</span></label></div><span class="clearfix"></span>
+</div>
+    <div class="element-select" title="Departamento"><label class="title"><span class="required">*</span></label><div class="item-cont"><div class="large"><span><select name="select" required="required">
+
+        <option value="Islas de la Bahía ">Islas de la Bahía </option>
+        <option value="Cortés ">Cortés </option>
+        <option value="Atlántida ">Atlántida </option>
+        <option value="Colón ">Colón </option>
+        <option value="Gracias a Dios ">Gracias a Dios </option>
+        <option value="Copán ">Copán </option>
+        <option value="Santa Bárbara ">Santa Bárbara </option>
+        <option value="Yoro ">Yoro </option>
+        <option value="Olancho ">Olancho </option>
+        <option value="Ocotepeque ">Ocotepeque </option>
+        <option value="Lempira ">Lempira </option>
+        <option value="Intibucá ">Intibucá </option>
+        <option value="Comayagua ">Comayagua </option>
+        <option value="Francisco Morazán ">Francisco Morazán </option>
+        <option value="El Paraíso ">El Paraíso </option>
+        <option value="La Paz ">La Paz </option>
+        <option value="Valle Choluteca">Valle Choluteca</option></select><i></i><span class="icon-place"></span></span></div></div></div>
+    <div class="element-select" title="Municipios"><label class="title"><span class="required">*</span></label><div class="item-cont"><div class="large"><span><select name="select1" required="required">
+
+        <option value="option 1">option 1</option>
+        <option value="option 2">option 2</option>
+        <option value="option 3">option 3</option></select><i></i><span class="icon-place"></span></span></div></div></div>
+    <div class="element-select" title="Grado de Escolaridad Integrante"><label class="title"></label><div class="item-cont"><div class="large"><span><select name="select2" >
+
+        <option value="Secundaria">Secundaria</option>
+        <option value="Universitaria">Universitaria</option>
+        <option value="Postgrado">Postgrado</option>
+        <option value="Doctorado">Doctorado</option>
+        <option value="Otro">Otro</option></select><i></i><span class="icon-place"></span></span></div></div></div>
+    <div class="element-email" title="Correo Electrónico"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="large" type="email" name="email" value="" required="required" placeholder="Correo Electrónico Integrante"/><span class="icon-place"></span></div></div>
+    <div class="element-phone" title="Teléfono Fijo"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="large" type="tel" pattern="[+]?[\.\s\-\(\)\*\#0-9]{3,}" maxlength="24" name="phone" required="required" placeholder="Teléfono Fijo Integrante" value=""/><span class="icon-place"></span></div></div>
+    <div class="element-phone" title="Teléfono Celular"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="large" type="tel" pattern="[+]?[\.\s\-\(\)\*\#0-9]{3,}" maxlength="24" name="phone1" required="required" placeholder="Teléfono Celular Integrante" value=""/><span class="icon-place"></span></div></div>
+    <div class="element-input" title="Dirección Exacta"><label class="title"><span class="required">*</span></label><div class="item-cont"><input class="large" type="text" name="input3" required="required" placeholder="Dirección Exacta Integrante"/><span class="icon-place"></span></div></div>
 <div class="submit"><input type="submit" value="Enviar"/></div></form><p class="frmd"><a href="">Validación Formulario</a> Hondurastartup MiPyme Form</p>
 <!-- Finaliza Formulario-->
 
