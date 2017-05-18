8f23197b4f6a5d27415e5b9c0dfc9f9b43b82970
Anwar Sarmiento <asarmiento@sistemasamigables.com>
2017-05-17 22:41:48 -0600

    cerrar sesion logout

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return redirect()->action('LoginController@logout');
+        return redirect()->action('Auth\LoginController@logout');
     }
 }
