0a8ab1f45f7763c82ce93be81e025d57f14a5248
Anwar Sarmiento <asarmiento@sistemasamigables.com>
2017-05-17 22:52:21 -0600

    cerrar sesion closed

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = 'http://robotschallenge.hondurastartup.com/';
 
     /**
      * Create a new controller instance.
