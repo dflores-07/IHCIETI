a310edbf6b1dba6ab0e7a031198768ec740a8d7c
David Flores <dflores@jed-itsolutions.com>
2017-05-22 11:15:02 -0600

    Formulario Nuevo

--- resources/views/Registro_Pitch.Blade.php
+++ resources/views/Registro_Pitch.Blade.php
@@ -37,6 +37,20 @@
                             </div>
                         </div>
 
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">Correo Electrónico</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
                          <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                             <label for="project-name" class="col-md-4 control-label">Nombre Proyecto</label>
 
@@ -52,7 +66,7 @@
                         </div>
 
                          <div class="form-group{{ $errors->has('project-description') ? ' has-error' : '' }}">
-                            <label for="project-description" class="col-md-4 control-label">Descripción del Proyecto</label>
+                            <label for="project-description" class="col-md-4 control-label">Contános sobre Tú Proyecto</label>
 
                             <div class="col-md-6">
                                 <input id="project-description" type="text" class="form-control" name="project-description" value="{{ old('project-description') }}" required >
@@ -71,64 +85,14 @@
                             <div class="input-group">
                                 <span class="input-group-addon"><i class="fa fa-map-marker"></i></span>
                                    <select name="select" required="required">
-                                        <option value="option 1">option 1</option>
-                                        <option value="option 2">option 2</option>
-                                        <option value="option 3">option 3</option>
+                                        <option value="option 1">Idea</option>
+                                        <option value="option 2">Desarrollo</option>
+                                        <option value="option 3">Implementado</option>
                                     </select>
                                 </span>
                             </div>
                         </div>
                      </div>
-
-
-                        <div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">Teléfono</label>
-
-                            <div class="col-md-6">
-                                <input id="phone" type="number" class="form-control" prefix="00000000" maxlength="8" minlength="8" name="phone" value="{{ old('phone') }}" required>
-
-                                @if ($errors->has('phone'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('phone') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">Correo Electrónico</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label"></label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="hidden" class="form-control" name="password" value="999999" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label"></label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="hidden" class="form-control" name="password_confirmation"  value="999999" required>
-                            </div>
-                        </div>
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
