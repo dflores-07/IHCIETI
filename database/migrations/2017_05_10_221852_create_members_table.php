909eae7356a8e9b08bf682bc00e3f2a5eb52775c
Anwar Sarmiento <asarmiento@sistemasamigables.com>
2017-05-30 16:50:18 -0600

    disenos da

--- database/migrations/2017_05_10_221852_create_members_table.php
+++ database/migrations/2017_05_10_221852_create_members_table.php
@@ -16,7 +16,7 @@ class CreateMembersTable extends Migration
         Schema::create('members', function (Blueprint $table) {
             $table->increments('id');
             $table->enum('type',['members','leader']);
-            $table->integer('idnumber');
+            $table->string('idnumber');
             $table->string('fname');
             $table->string('flname');
             $table->string('slname');
