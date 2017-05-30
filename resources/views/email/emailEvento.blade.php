c9d44b82562abf6525cd848979baea91fcbef58f
David Flores <dflores@jed-itsolutions.com>
2017-05-30 11:56:31 -0600

    Mensaje de Bienvenida y Correo

--- resources/views/email/emailEvento.blade.php
+++ resources/views/email/emailEvento.blade.php
@@ -10,11 +10,11 @@
 
 <div class="content">
     <div class="text-center row"><h2>Bienvenid@ : {{$profile->name}}</h2></div>
-    <div class="text-center row"><img src="http://robotschallenge.hondurastartup.com/wp-content/uploads/2017/05/logo.png" width="350" height="150"></div>
+    <div class="text-center row"><img src="http://mipymes.hondurastartup.com/wp-content/uploads/2017/05/logotipo.png" width="350" height="150"></div>
 
-    <div class="text-center row"><h3>En base a la metodología de selección esperamos..</h3></div>
-
-    	<div class="text-center row"><h4>Que seas un afortunado para presentar Tu Pitch..</h4></div>
+    <div class="text-center row"><h3>Bienvenid@!! has sido registrado correctamente!.</h3></div>
+    	<div class="text-center row"><h3>Tu numero de proyecto es : {{$profile->idproject}}</h3></div>
+    	<div class="text-center row"><h4>Pronto estaremos comunicandonos contigo..</h4></div>
 
 
 </div>
--- resources/views/proyects/finish.blade.php
+++ resources/views/proyects/finish.blade.php
@@ -7,4 +7,18 @@
  * Time: 02:53
  *-->
 
-echo 'aqui debes poner un diseno de finalizado la inscripcion del projecto';
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+          <div class="panel panel-default">
+            <div class="text-center row"><h2>Bienvenid@ : {{$profile->name}}</h2></div>
+    		<div class="text-center row"><img src="http://mipymes.hondurastartup.com/wp-content	/uploads/2017/05/logotipo.png" width="350" height="150"></div>
+    			<div class="text-center row"><h3>Tu numero de proyecto es : {{$profile->idproject}}</h3></div>
+    	<div class="text-center row"><h4>Hemos enviado un mensaje de confirmación a tu correo electrónico..</h4></div>
+                
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
